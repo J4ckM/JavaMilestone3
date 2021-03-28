ddce07627caf3bfa87964fb1ff36ec04b162f317
J4ckM <Jack.Murphy.2@city.ac.uk>
2021-03-28 17:51:05 +0100

    Create PlatformBlue.java

---
+++ MyGameProject/src/game/Structure/PlatformBlue.java
@@ -0,0 +1,18 @@
+package game.Structure;
+
+import city.cs.engine.BoxShape;
+import city.cs.engine.StaticBody;
+import city.cs.engine.World;
+import org.jbox2d.common.Vec2;
+
+import java.awt.*;
+
+public class PlatformBlue extends Platform {
+
+    public PlatformBlue(World w, float width, float height, float x, float y){
+        //super(w, new BoxShape(width, height));
+        super(w, width, height, x, y);
+        setPosition(new Vec2(x,y));
+        setFillColor(Color.blue);
+    }
+}
