45b4915cd507e6ba21fac25d20ef821e4b5fe26e
J4ckM <Jack.Murphy.2@city.ac.uk>
2021-03-28 17:51:31 +0100

    Create GiveFocus.java

---
+++ MyGameProject/src/game/World/GiveFocus.java
@@ -0,0 +1,42 @@
+package game.World;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import game.Avatar.*;
+import game.Entity.*;
+import game.Structure.*;
+
+public class GiveFocus implements MouseListener {
+
+    private GameView view;
+
+    public GiveFocus(GameView v){
+        this.view = v;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        view.requestFocus();
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
